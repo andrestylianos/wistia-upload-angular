719561497dfc2ad2d048e60375aa0dd184208f35
André Ramos <andre.stylianos@gmail.com>
2016-01-06 18:25:25 -0200

    yeoman scaffold with generator-gulp-angular

---
+++ src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('processInterviewTest')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('processInterviewTest', ['ngResource', 'ui.router', 'ui.bootstrap', 'toastr']);
+
+})();
