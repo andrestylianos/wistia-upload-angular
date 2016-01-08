fc25ecb52f4ab19bba53f9d24a2c44227661eec4
André Ramos <andre.stylianos@gmail.com>
2016-01-08 04:31:56 -0200

    config firebase hosting

--- src/app/components/pitembed/pitembed.directive.js
+++ src/app/components/pitembed/pitembed.directive.js
@@ -10,7 +10,7 @@
     var directive = {
       restrict: 'E',
       template: function(){
-        return '<iframe src="http://fast.wistia.net/embed/iframe/'+$stateParams.mediaid+'" allowtransparency="true" frameborder="0" scrolling="no" class="wistia_embed" name="wistia_embed" width="640" height="360"></iframe>'
+        return '<iframe src="https://fast.wistia.net/embed/iframe/'+$stateParams.mediaid+'" allowtransparency="true" frameborder="0" scrolling="no" class="wistia_embed" name="wistia_embed" width="640" height="360"></iframe>'
       }
     };
     return directive;
